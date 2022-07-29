6e47fa31d9abe9d85b6b2bff01224a3b44b634e1
Matthias Volk <m.volk@utwente.nl>
2022-07-29 15:52:56 +0200

    Dockerfile for storm-basesystem

---
+++ storm-basesystem/build_all_storm-basesystem.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+# Build all images of Linux systems currently supporting Storm
+##############################################################
+
+# Build Ubuntu 18.04 LTS "Bionic Beaver"
+echo "### Building Ubuntu 18.04 base..."
+docker build --no-cache --pull -t movesrwth/storm-basesystem:ubuntu-18.04 --build-arg LINUX_BASE=ubuntu:18.04 .
+docker push movesrwth/storm-basesystem:ubuntu-18.04
+echo "### Building Ubuntu 18.04 base finished."
+
+# Build Debian 10 "Buster"
+echo "### Building Debian 10 base..."
+docker build --no-cache --pull -t movesrwth/storm-basesystem:debian-10 --build-arg LINUX_BASE=debian:10 .
+docker push movesrwth/storm-basesystem:debian-10
+echo "### Building Debian 10 base finished."
+
+# Build Ubuntu 20.04 LTS "Focal Fossa"
+echo "### Building Ubuntu 20.04 base..."
+docker build --no-cache --pull -t movesrwth/storm-basesystem:ubuntu-20.04 --build-arg LINUX_BASE=ubuntu:20.04 .
+docker push movesrwth/storm-basesystem:ubuntu-20.04
+echo "### Building Ubuntu 20.04 base finished."
+
+# Build Debian 11 "Bullseye"
+echo "### Building Debian 11 base..."
+docker build --no-cache --pull -t movesrwth/storm-basesystem:debian-11 --build-arg LINUX_BASE=debian:11 .
+docker push movesrwth/storm-basesystem:debian-11
+echo "### Building Debian 11 base finished."
+
+# Build Ubuntu 22.04 LTS "Jammy Jellyfish"
+echo "### Building Ubuntu 22.04 base..."
+docker build --no-cache --pull -t movesrwth/storm-basesystem:ubuntu-22.04 --build-arg LINUX_BASE=ubuntu:22.04 .
+docker push movesrwth/storm-basesystem:ubuntu-22.04
+echo "### Building Ubuntu 22.04 base finished."
+
+# Build latest Ubuntu release version
+echo "### Building latest Ubuntu base..."
+docker build --no-cache --pull -t movesrwth/storm-basesystem:latest .
+docker push movesrwth/storm-basesystem:latest
+echo "### Building latest Ubuntu base finished."
