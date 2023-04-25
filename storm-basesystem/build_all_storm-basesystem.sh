b68b2b25aa6dc8205f0dac27b64f47d7d7c841b3
Matthias Volk <m.volk@utwente.nl>
2023-04-25 13:45:53 +0200

    Removed ruby dependency for storm-basesystem and added support for Ubuntu 23.04

--- storm-basesystem/build_all_storm-basesystem.sh
+++ storm-basesystem/build_all_storm-basesystem.sh
@@ -4,12 +4,6 @@ set -e
 # Build all images of Linux systems currently supporting Storm
 ##############################################################
 
-# Build Ubuntu 18.04 LTS "Bionic Beaver"
-echo "### Building Ubuntu 18.04 base..."
-docker build --no-cache --pull -t movesrwth/storm-basesystem:ubuntu-18.04 --build-arg LINUX_BASE=ubuntu:18.04 .
-docker push movesrwth/storm-basesystem:ubuntu-18.04
-echo "### Building Ubuntu 18.04 base finished."
-
 # Build Debian 10 "Buster"
 echo "### Building Debian 10 base..."
 docker build --no-cache --pull -t movesrwth/storm-basesystem:debian-10 --build-arg LINUX_BASE=debian:10 .
@@ -33,6 +27,12 @@ echo "### Building Ubuntu 22.04 base..."
 docker build --no-cache --pull -t movesrwth/storm-basesystem:ubuntu-22.04 --build-arg LINUX_BASE=ubuntu:22.04 .
 docker push movesrwth/storm-basesystem:ubuntu-22.04
 echo "### Building Ubuntu 22.04 base finished."
+
+# Build Ubuntu 23.04 "Lunar Lobster"
+echo "### Building Ubuntu 23.04 base..."
+docker build --no-cache --pull -t movesrwth/storm-basesystem:ubuntu-23.04 --build-arg LINUX_BASE=ubuntu:23.04 .
+docker push movesrwth/storm-basesystem:ubuntu-23.04
+echo "### Building Ubuntu 23.04 base finished."
 
 # Build latest Ubuntu release version
 echo "### Building latest Ubuntu base..."
