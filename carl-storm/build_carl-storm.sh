deb5704b1a9fa10ac1d53a0db98789282800ad18
Matthias Volk <m.volk@utwente.nl>
2023-05-02 10:49:10 +0200

    Updated build scripts for carl-storm

--- carl-storm/build_carl-storm.sh
+++ carl-storm/build_carl-storm.sh
@@ -4,14 +4,18 @@ set -e
 # Build images for Carl-storm library
 #####################################
 
-version=14.24
+version=14.25
+
+echo "### Clone repository..."
+git clone https://github.com/moves-rwth/carl-storm -b $version
+cd carl-storm
 
 echo "### Building docker image for Carl-storm $version ..."
-docker build --no-cache --pull --build-arg carl_branch=$version --build-arg build_type=Release --build-arg BASE_IMAGE=movesrwth/storm-basesystem:latest -t movesrwth/carl-storm:$version .
-docker push movesrwth/carl-storm:$version
+#docker build . --no-cache --pull --build-arg build_type=Release --build-arg BASE_IMAGE=movesrwth/storm-basesystem:latest -t movesrwth/carl-storm:$version
+#docker push movesrwth/carl-storm:$version
 echo "### Building docker image for Carl-storm $version finished."
 
 echo "### Building docker image for Carl-storm $version in debug configuration ..."
-docker build --no-cache --pull --build-arg carl_branch=$version --build-arg build_type=Debug --build-arg BASE_IMAGE=movesrwth/storm-basesystem:latest -t movesrwth/carl-storm:$version-debug .
-docker push movesrwth/carl-storm:$version-debug
+docker build . --no-cache --pull --build-arg build_type=Debug --build-arg BASE_IMAGE=movesrwth/storm-basesystem:latest -t movesrwth/carl-storm:$version-debug
+#docker push movesrwth/carl-storm:$version-debug
 echo "### Building docker image for Carl-storm $version in debug configuration finished."
