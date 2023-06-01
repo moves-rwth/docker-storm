9e501fda5b43f5913302f0a7134bb2af069ed67c
Matthias Volk <m.volk@utwente.nl>
2023-06-01 12:08:37 +0200

    Updated build script for carl-storm

--- carl-storm/build_carl-storm.sh
+++ carl-storm/build_carl-storm.sh
@@ -11,11 +11,11 @@ git clone https://github.com/moves-rwth/carl-storm -b $version
 cd carl-storm
 
 echo "### Building docker image for Carl-storm $version ..."
-#docker build . --no-cache --pull --build-arg build_type=Release --build-arg BASE_IMAGE=movesrwth/storm-basesystem:latest -t movesrwth/carl-storm:$version
-#docker push movesrwth/carl-storm:$version
+docker build . --no-cache --pull --build-arg build_type=Release --build-arg BASE_IMAGE=movesrwth/storm-basesystem:latest -t movesrwth/carl-storm:$version
+docker push movesrwth/carl-storm:$version
 echo "### Building docker image for Carl-storm $version finished."
 
 echo "### Building docker image for Carl-storm $version in debug configuration ..."
 docker build . --no-cache --pull --build-arg build_type=Debug --build-arg BASE_IMAGE=movesrwth/storm-basesystem:latest -t movesrwth/carl-storm:$version-debug
-#docker push movesrwth/carl-storm:$version-debug
+docker push movesrwth/carl-storm:$version-debug
 echo "### Building docker image for Carl-storm $version in debug configuration finished."
